aaa804b27f89fe040bf9e951ce636d10b5c9df2d
Pranathi <89980698+Kairamkondapranathi@users.noreply.github.com>
2021-11-16 13:19:57 +0530

    Nov9

---
+++ MoveZerosToEnd.java
@@ -0,0 +1,25 @@
+package assignments;
+
+public class MoveZerosToEnd {
+	public static void main(String[] args) {
+		int a[] = {0,1,1,0,0,0,1,0};
+		int start =0;
+		int end = a.length-1;
+		
+		while(start<=end) {
+			if(a[start] == 1) {
+				int temp = a[start];
+				a[start] = a[end];
+				a[end] = temp;
+				end--;
+			}
+			else 
+				start++;
+		}
+		
+		for(int i : a) System.out.print(i+" ");
+	}
+
+}
+
+
